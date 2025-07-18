a5b81ef2cf9af680b81010a78156e9c7fe539cd7
charushrestha379 <charushrestha776@gmail.com>
2025-07-18 14:44:34 +0545

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,14 @@
+/* 
+string = 'a' "b"
+number = 1, 34.5, -2
+boolean = true, false
+undefined = undefined value (condition where an expression does not have a correct value, although it is syntactically correct)
+null = it is used when you explicitly set something as nothing(no value)
+*/
+let name = "Charu"
+let age = 25
+let haspassed = true
+let address = null
+let x //undefined
+
+console.log(name, age, haspassed, address, x)
---
+++ output-comment.js
@@ -0,0 +1,7 @@
+const a = "apple";
+console.log("Hello, world!", 3, a)
+console.error("error")
+console.warn("warning")
+//single line comment
+/* multi line 
+comment */
