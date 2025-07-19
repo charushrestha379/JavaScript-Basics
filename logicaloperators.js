8fc83795787dc5f61ad2d9c0f18f0686dce13a02
charushrestha379 <charushrestha776@gmail.com>
2025-07-19 18:10:54 +0545

    logical operators

---
+++ logicaloperators.js
@@ -0,0 +1,17 @@
+/*
+&& //returns true if both operands are true
+|| //returns true if at least one operand is true
+! //returns true if the operand is false, and false if the operand is true
+
+*/ 
+console.log(true && true) // true
+console.log(true && false) //false
+console.log(false && true) //false
+console.log(false && true)      //false
+console.log(false && false) //false
+console.log(true || true) //true
+console.log(true || false) //true
+console.log(false || true) //true
+console.log(false || false) //false   
+console.log(!true) //false
+console.log(!false) //true
