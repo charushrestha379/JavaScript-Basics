f690e191bdd690e3a0cc0a2f6903229c8363fc05
charushrestha379 <charushrestha776@gmail.com>
2025-07-19 19:29:19 +0545

    if else if

---
+++ if-else.js
@@ -0,0 +1,17 @@
+// if else
+if (false)
+    console.log("This will not run")
+else
+    console.log("This will run")
+
+//if else if 
+if (true)
+    console.log("This will run")
+else if (false)
+    console.log("This will not run")
+else
+    console.log("This will also not run")
+
+//ternary condition or one liner condition
+const cond = 3 < 2 ? "cool" : "not cool"
+console.log(cond) // prints "not cool"
