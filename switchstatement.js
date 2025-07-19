945f9610ac29bfa4a870cbc952aaa7391ac6504b
charushrestha379 <charushrestha776@gmail.com>
2025-07-19 19:38:26 +0545

    switch example

---
+++ switchstatement.js
@@ -0,0 +1,16 @@
+const value = 1
+
+switch (value) {
+    case 1:
+    console.log("1 is not cool")
+    break;
+   case 3: 
+   console.log("3 is cool")
+   break;
+   case 4:
+    console.log("4 is not cool")
+    break;
+    default:
+        console.log("okay")
+        break; 
+}
