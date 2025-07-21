b12c1c9e9f1cc85390c94095458f4fd852193003
charushrestha379 <charushrestha776@gmail.com>
2025-07-21 17:05:46 +0545

    while loop and do while loop

---
+++ dowhileloop.js
@@ -0,0 +1,21 @@
+//do while loop executes a block of code once, and then repeats the loop as long as a specified condition evaluates to true.
+do {
+    console.log("run")
+    break
+} while(true)
+
+//executes a block of code and then checks the condition
+
+let num = 1
+do {
+    console.log("count is:",num)
+    num++
+}while (num <=5 )
+
+ //while loop is similar to do-while but checks the condition before executing the block of code   
+
+ 
+while (true) {
+    console.log("run")
+break
+}
