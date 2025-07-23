defebdbbca72c4c55950b3dd383d809b8c3f8945
charushrestha379 <charushrestha776@gmail.com>
2025-07-23 17:24:48 +0545

    about objects

--- objects.js
+++ objects.js
@@ -21,3 +21,28 @@ info.arr = [1, 2  ]
 
 console.log(info)
 console.log(info.career)
+//to show values only
+console.log(Object.values(info))
+console.log(Object.keys(info.career))
+//to show keys only 
+console.log(Object.keys(info))
+//to show both keys and values
+console.log(Object.entries(info))
+
+
+//combining objects
+const info2 = {
+    hairColor: "black",
+    age: 20,
+    arr2: [1, 2, 3]
+}
+
+const info3 = {...info, ...info2}
+console.log(info3)
+info2.hairColor = "brown"
+info2.arr2.push("hello")
+console.log(info, info2)
+
+//destructuring objects is 
+const {hairColor, name} = info3
+console.log(hairColor, name)
